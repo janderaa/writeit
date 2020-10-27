5ed947dd7f2855beadb2f10cc3dcb75b835365cc
janderaa <workjorge@yahoo.com>
2020-10-26 23:49:27 -0500

    var for textGen, key event improved

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var textGen = null;
+
 class TextGenerator{
 
     constructor(data){
@@ -7,12 +9,13 @@ class TextGenerator{
         this.content = Object.values(Object.values(data)[0])[3];
         this.length = this.content.length;
         this.contentArray = this.content.split('');
-        this.tempS = this.title+" "+this.url+" "+this.content+" "+this.length+" "+this.contentArray;
+        //this.tempS = this.title+" "+this.url+" "+this.content+" "+this.length+" "+this.contentArray;
     }
     show_content(){
         const resultContent = document.getElementById("text-rw");
         //resultContent.insertAdjacentHTML('beforeend',data);
         //resultContent.innerHTML = this.tempS;
+        console.info(this.contentArray);
         resultContent.innerHTML = this.content;
     }
 
@@ -78,9 +81,10 @@ class APIManager{
             break;
         }
         
-    }
-   
+    }  
 }
+
+
 
 const form = document.getElementById("search-form").addEventListener("submit",async (event)=>{
     const query = document.getElementById("search-input").value; 
@@ -91,7 +95,7 @@ const form = document.getElementById("search-form").addEventListener("submit",as
         const reqContent = new APIManager('action=query&prop=extracts&explaintext=&format=json&&origin=*&exintro=1&titles='+data.title,"content");
         const dataCont = await reqContent.handle_query(event);
         if(dataCont){
-            const textGen = new TextGenerator(dataCont);
+            textGen = new TextGenerator(dataCont);
             textGen.show_content();
             console.log(dataCont);
             
@@ -99,12 +103,58 @@ const form = document.getElementById("search-form").addEventListener("submit",as
             console.error("data content not found!");
         }
     }else{
-        console.error("data not found!")
+        console.error("data not found!");
     }
 });
 
-window.onkeypress = ()=>{
-    // it always called whenever the user types something
-    // to DO: detect key, detect special case keys (shift+1, shift+`, etc..)
-    console.log("Key pressed");
+window.onkeydown = (e)=>{
+    var ctrl = e.ctrlKey ? true : false;
+    var shift = e.shiftKey ? true : false; 
+    // explore textGen contentArray
+    // identify the first letter on the array
+    // if it coincides with the key pressed*  then put it out of the array 
+    // AND make it known in the text on the screen
+    switch(e.key){
+        case "a":
+        case "c":
+        case "d":
+        case "e":
+        case "f":
+        case "g":
+        case "h":
+        case "i":
+        case "j":
+        case "k":
+        case "l":
+        case "m":
+        case "n":
+        case "ñ":
+        case "o":
+        case "p":
+        case "q":
+        case "r":
+        case "s":
+        case "t":
+        case "u":
+        case "v":
+        case "w":
+        case "x":
+        case "y":
+        case "z":
+        
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+
+        case " ":
+
+    }
+
 }
