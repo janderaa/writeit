f4e61e4bcd6fd5c1c1a49c07ec1e126de0f56105
ldJand <workjorge@yahoo.com>
2021-03-28 18:08:06 -0500

    Array filter for odd characters (untested)

--- textManager.js
+++ textManager.js
@@ -2,14 +2,15 @@
 
 class textGenManager{
     constructor(data){
-
         this.title = Object.values(Object.values(data)[0])[2];
         this.url = `https://en.wikipedia.org/?curid=`+Object.values(Object.values(data)[0])[0];
         this.content = Object.values(Object.values(data)[0])[3];
         this.length = this.content.length;
         this.contentArray = this.content.split('');
+        this.process_content()
         //this.tempS = this.title+" "+this.url+" "+this.content+" "+this.length+" "+this.contentArray;
     }
+
     show_content(){
         const resultContent = document.getElementById("text-rw");
         //resultContent.insertAdjacentHTML('beforeend',data);
@@ -19,14 +20,19 @@ class textGenManager{
         resultContent.innerHTML = this.content;
     }
 
-    hide_content(){
-
-    }
-    
+    /*
+    Method to filter odd characters on contentArray <Array>
+    */
+    process_content(){
+        this.contentArray.map(data => {
+            if(data==="–")
+                data = "-"
+        });
+    }    
+ 
+    // Method to update user-written characters
     hide_character(){
-        
         this.content = this.content.slice(1);
-        
         this.contentArray.shift();
         this.show_content();
     }
